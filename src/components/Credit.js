9a676111c230a9554799afd2c039b43084bc409e
React-Weather-App-main <harshvardhanchaudhari443@gmail.com>
2024-10-11 12:28:58 +0530

    First Commit

---
+++ src/components/Credit.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://github.com/harsh-code12/HarshCode"> Harshvardhan Chaudhari </a>
+         Open sourced on GitHub
+    </p>
+  </div>
+  );
+}
