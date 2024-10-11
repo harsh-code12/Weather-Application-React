81bbe984f38c3421018176646790ac6156d18319
harsh-code12 <harshvardhanchaudhari443@gmail.com>
2024-10-11 12:42:10 +0530

    Credit.js

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -5,7 +5,7 @@ export default function Credit() {
     <div class="credit">
     <p>
       Coded by
-      <a href="https://github.com/harsh-code12/HarshCode"> Harshvardhan Chaudhari </a>
+      <a href="https://github.com/harsh-code12/Weather-Application-React.git"> Harshvardhan Chaudhari </a>
          Open sourced on GitHub
     </p>
   </div>
